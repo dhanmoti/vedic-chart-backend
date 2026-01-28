ab24a52ec49d2561300aa3a30d7555bf442da574
Dhan Moti (Devi) <48517418+dhanmoti@users.noreply.github.com>
2026-01-28 12:10:50 +0800

    setup firebase project

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const {logger} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/v2/https");
-
-exports.helloVedic = onRequest((request, response) => {
-  logger.info("helloVedic function invoked", {structuredData: true});
-  response.json({
-    message: "Namaste from Firebase Functions!",
-  });
-});
